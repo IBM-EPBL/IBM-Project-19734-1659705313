0e7566de17c07aa75c1d9cdf14a96d3e9571fef7
SNEHA L <kasilakshmanan3@gmail.com>
2022-11-18 00:30:33 +0530

    Web Application

--- Project development phase/Sprint 3/app.py
+++ Project development phase/Sprint 3/app.py
@@ -12,14 +12,14 @@ from werkzeug.utils import secure_filename
 global graph
 graph=tf.compat.v1.get_default_graph()
 #this list is used to log the predictions in the server console
-predictions = ["Corpse Flower", 
-               "Great Indian Bustard", 
-               "Lady's slipper orchid", 
-               "Pangolin", 
-               "Spoon Billed Sandpiper", 
+predictions = ["Corpse Flower",
+               "Great Indian Bustard",
+               "Lady's slipper orchid",
+               "Pangolin",
+               "Spoon Billed Sandpiper",
                "Seneca White Deer"
               ]
-#this list contains the link to the predicted species              
+#this list contains the link to the predicted species
 found = [
         "https://en.wikipedia.org/wiki/Amorphophallus_titanum",
         "https://en.wikipedia.org/wiki/Great_Indian_bustard",
@@ -33,7 +33,16 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     # Home Page
+    return render_template("login.html")
+@app.route('/signup', methods=['GET'])
+def index1():
+    # Home Page
+    return render_template("signup.html")
+@app.route('/home', methods=['GET'])
+def index2():
+    # Home Page
     return render_template("index.html")
+
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == 'GET':
@@ -42,28 +51,30 @@ def upload():
         # Fetching the uploaded image from the post request using the id 'uploadedimg'
         f = request.files['uploadedimg']
         basepath = os.path.dirname(__file__)
+        print(basepath)
         #Securing the file by creating a path in local storage
         file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
+        print(file_path)
         #Saving the uploaded image locally
         f.save(file_path)
-        #loading the locally saved image 
+        #loading the locally saved image
         img = tf.keras.utils.load_img(file_path, target_size=(224, 224))
-        #converting the loaded image to image array 
+        #converting the loaded image to image array
         x = tf.keras.utils.img_to_array(img)
         x = preprocess_input(x)
         # Converting the preprecessed image to numpy array
         inp = np.array([x])
         with graph.as_default():
             #loading the saved model from training
-            json_file = open('C:/Users/0xluk/OneDrive/Desktop/Project Development Phase/Sprint 2/DigitalNaturalist.json', 'r')
+            json_file = open("DigitalNaturalist.json", 'r')
             loaded_model_json = json_file.read()
             json_file.close()
             loaded_model = model_from_json(loaded_model_json)
             #adding weights to the trained model
-            loaded_model.load_weights("C:/Users/0xluk/OneDrive/Desktop/Project Development Phase/Sprint 2/DigitalNaturalist.h5")
+            loaded_model.load_weights("DigitalNaturalist.h5")
             #predecting the image
             preds =  np.argmax(loaded_model.predict(inp),axis=1)
-            #logs are printed to the console 
+            #logs are printed to the console
             print("Predicted the Species " + str(predictions[preds[0]]))
         text = found[preds[0]]
         return redirect(text)
